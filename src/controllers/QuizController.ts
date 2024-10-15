361ccaa96ae78775f46f8de0643b652f26cf605b
msouza28 <matheus.emiliano28@gmail.com>
2024-10-15 17:55:40 -0300

    Novo ajuste no log de erro do metodo inserir quiz

--- src/controllers/QuizController.ts
+++ src/controllers/QuizController.ts
@@ -22,7 +22,11 @@ export class QuizController {
             if (error instanceof QueryFailedError && error.message.includes('duplicate key value violates unique constraint')) {
                 return res.status(409).json({ message: "Um quiz ja foi respondido para esse usuário"  });
             }
-            res.status(500).json({ message: "Erro ao criar quiz do usuário", error });
+            // Logando a mensagem de erro no caso de erro 500
+            const errorMessage = error instanceof Error ? error.message : "Erro desconhecido";
+            console.error(`Erro ao criar quiz do usuário: ${errorMessage}`);
+
+            res.status(500).json({ message: "Erro ao criar quiz do usuário", error: errorMessage });
         }
     };
 }
